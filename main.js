bb13e13a1653421d82547e11fc01e64d92c98871
giga <gmadzgharashvili@gmail.com>
2022-09-08 14:46:14 +0400

    ...

---
+++ main.js
@@ -0,0 +1,14 @@
+//1
+
+// let array = ["academy", "of", "digital", "industries"].reduce(function (x, y) {
+//   return x + " ".concat(y);
+// }, "");
+
+// console.log(array);
+
+//2
+
+// let array =[23,45,32,5,87,7,3,98];
+
+// let minValue = array.sort((x, y) => y-x)[array.length -1];
+// console.log(minValue);
